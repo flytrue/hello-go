bc8600c886a7ad38addaa702b7ebff13b18b18ac
flytrue <40475038+flytrue@users.noreply.github.com>
2019-10-15 17:26:57 +0300

    fix errors

--- main.go
+++ main.go
@@ -49,6 +49,25 @@ func addOrder(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
+	// switch r.Method {
+	// case "GET":
+	// 	http.ServeFile(w, r, "form.html")
+	// case "POST":
+	// 	// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+	// 	if err := r.ParseForm(); err != nil {
+	// 		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	// 		return
+	// 	}
+	// 	fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+	// 	name := r.FormValue("name")
+	// 	address := r.FormValue("address")
+	// 	fmt.Fprintf(w, "Name = %s\n", name)
+	// 	fmt.Fprintf(w, "Address = %s\n", address)
+	// default:
+	// 	fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	// }
+	// return
+
 	fmt.Println("New order for User Endpoint Hit")
 
 	vars := mux.Vars(r)
@@ -70,7 +89,7 @@ func addOrder(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(productcount, " = ", productcountint)
 
 	dbc := db.Create(&Order{UserID: userid, DateDelivery: nil, ProductId: productidint, ProductCount: productcountint})
-	fmt.Println("start create" + dbc.Error.Error())
+
 	if dbc.Error != nil {
 		fmt.Fprintf(w, "Error create = "+dbc.Error.Error())
 		return
@@ -178,8 +197,9 @@ func handleRequests() {
 	myRouter.HandleFunc("/user/{name}", deleteUser).Methods("DELETE")
 	myRouter.HandleFunc("/user/{name}/{email}/{iduser}", updateUser).Methods("PUT")
 	myRouter.HandleFunc("/user/{name}/{email}/{iduser}", newUser).Methods("POST")
-	myRouter.HandleFunc("/orders", allOrders).Methods("GET")
-	myRouter.HandleFunc("/order/{userid}/{productid}/{productcount}", addOrder).Methods("POST")
+	//myRouter.HandleFunc("/orders", allOrders).Methods("GET")
+	myRouter.HandleFunc("/orders", allOrders).Methods("POST")
+	myRouter.HandleFunc("/order/{userid}/{productid}/{productcount}", addOrder)
 
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
